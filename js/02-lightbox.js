096b2b200e82a117bd53b52d588729d72e0d22b4
MarynaKindras <maryna.kindras@gmail.com>
2023-10-21 14:59:22 +0300

    Main commit

---
+++ js/02-lightbox.js
@@ -0,0 +1,25 @@
+import { galleryItems } from "./gallery-items.js";
+
+const refs = {
+  listGallery: document.querySelector(".gallery"),
+};
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}"/>
+        </a>
+        </li>`
+    )
+    .join("");
+}
+
+refs.listGallery.innerHTML = createMarkup(galleryItems);
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
